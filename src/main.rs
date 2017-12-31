201ed1a4c556f4b1e005356c15e8a942e99ee0a2
foudfou <foudil.newbie+git@gmail.com>
2017-12-31 03:18:37 +0100

    Add h2b command to main binary

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ extern crate cryptopals;
 
 use std::env;
 use std::fs::File;
-use std::io::Read;
+use std::io;
+use std::io::{Read, Write};
 
 use getopts::Options;
-use cryptopals::xor;
+use cryptopals::{b64,xor};
 
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} [options] FILE", program);
@@ -18,8 +19,9 @@ fn main() {
     let program = args[0].clone();
 
     let mut opts = Options::new();
-    // opts.optopt("o", "", "set output file name", "NAME");
-    opts.optflag("h", "help", "print this help menu");
+    opts.optopt("c", "cmd", "Command, one of {xorguess,h2b}.
+                 Defaults to xorguess", "CMD");
+    opts.optflag("h", "help", "Print this help menu");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
         Err(f) => { panic!(f.to_string()) }
@@ -29,21 +31,32 @@ fn main() {
         return;
     }
 
-    // let output = matches.opt_str("o");
-    let filename = if !matches.free.is_empty() {
-        matches.free[0].clone()
-    } else {
+    let cmd = &matches.opt_str("c").unwrap_or("xorguess".to_string());
+    if matches.free.is_empty() {
         print_usage(&program, opts);
         return;
     };
+    let filename = matches.free[0].clone();
 
-    let mut file = File::open(filename).expect("file not found");
-    let mut cipher: Vec<u8> = Vec::new();
-    file.read_to_end(&mut cipher)
-        .expect("something went wrong reading the file");
-    for possible_key in xor::guess_xor(&cipher) {
-        let k = String::from_utf8(possible_key).unwrap();
-        println!("{}", k);
+    if cmd == "xorguess" {
+        let mut file = File::open(filename).expect("file not found");
+        let mut cipher: Vec<u8> = Vec::new();
+        file.read_to_end(&mut cipher)
+            .expect("something went wrong reading the file");
+        for possible_key in xor::guess_xor(&cipher) {
+            let k = String::from_utf8(possible_key).unwrap();
+            println!("{}", k);
+        }
+    }
+    else if cmd == "h2b" {
+        let mut file = File::open(filename).expect("file not found");
+        let mut content = String::new();
+        file.read_to_string(&mut content)
+            .expect("something went wrong reading the file");
+        let bin = b64::hex2bytes(content.trim_right().to_string())
+            .expect("could not parse hex");
+        let mut stdout = io::stdout();
+        stdout.write_all(&bin).expect("I/O error");
     }
 
 }
