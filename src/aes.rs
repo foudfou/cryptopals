4bfe799bb439e9bb5a4d382d5c1515a257836635
foudfou <foudil.newbie+git@gmail.com>
2018-01-20 23:43:02 +0100

    Set1/Chal8: Detect AES in ECB mode

--- src/aes.rs
+++ src/aes.rs
@@ -1,9 +1,31 @@
+use std::collections::HashMap;
+
+pub fn detec_ecb(input: &[u8], keylen: usize) -> bool {
+    let mut blocks: HashMap<&[u8], i32> = HashMap::new();
+    let mut identical = 0;
+
+    for block in input.chunks(keylen) {
+        if blocks.contains_key(block) {
+            identical += 1;
+        }
+        else {
+            blocks.insert(block, 1);
+        }
+    }
+
+    return identical > 0;
+}
+
 
 #[cfg(test)]
 mod tests {
-    use b64;
-
+    use std::fs::File;
+    use std::io::{BufReader,BufRead};
     use openssl::symm::{decrypt, Cipher};
+
+    use b64;
+    use b64::{hex2bytes};
+    use super::*;
 
     #[test]
     fn test_aes_decrypt() {
@@ -16,6 +38,34 @@ mod tests {
         // let pl = String::from_utf8(plain).unwrap();
         let head = b"I'm back and I'm ringin' the bell";
         assert_eq!(&head[..], &plain[0..33]);
+    }
+
+    #[test]
+    fn test_detect_ecb() {
+        // let plain = "abcdefghijklmnopabcdefghijklmnop";
+        // let key = b"YELLOW SUBMARINE";
+        let aes_128_ecb_encoded =
+          b"\xbd\xb1\x84\xd4\x4e\x1f\xc1\xd3\x06\x09\x45\xb5\x3c\x99\x4f\x48\
+            \xbd\xb1\x84\xd4\x4e\x1f\xc1\xd3\x06\x09\x45\xb5\x3c\x99\x4f\x48\
+            \x60\xfa\x36\x70\x7e\x45\xf4\x99\xdb\xa0\xf2\x5b\x92\x23\x01\xa5";
+        assert!(detec_ecb(aes_128_ecb_encoded, 16));
+    }
+
+    #[test]
+    fn test_detect_ecb_sample() {
+        let file = File::open("data/8.txt").unwrap();
+        let mut found: Vec<i32> = Vec::new();
+        let mut line_idx = 0;
+        for line in BufReader::new(file).lines() {
+            line_idx += 1;
+            let raw = hex2bytes(line.unwrap()).unwrap();
+            let ding = detec_ecb(&raw, 16);
+            if ding {
+                found.push(line_idx);
+            }
+        }
+        assert_eq!(found.len(), 1);
+        assert_eq!(found[0], 133);
     }
 
 }
