e46d65c31b6e16b7adc78b1f6b81640d084a3552
foudil <username@users.noreply.github.com>
2020-01-02 05:42:14 +0100

    Set3/Chall23: Clone an MT19937 RNG from its output

--- src/rng.rs
+++ src/rng.rs
@@ -32,8 +32,8 @@ impl MT19937 {
             /* only MSBs of the array mt[].                        */
             /* 2002/01/09 modified by Makoto Matsumoto             */
         }
-        // F**ing C post-increment! The orinal code is `for (mti=1; mti<N;
-        // mti++)` ...which leaves mti at N!
+        // F**ing C post-increment! The orinal code is `for (mti=1; mti<N; mti++)`
+        // ...which leaves mti at N!
         rng.mti = N;
 
         rng
@@ -50,15 +50,9 @@ impl MT19937 {
         }
 
         /* Tempering */
-        let mut y = self.mt[self.mti];
+        let y = self.mt[self.mti];
         self.mti += 1;
-
-        y ^= y >> 11;
-        y ^= (y << 7) & 0x9d2c5680u32;
-        y ^= (y << 15) & 0xefc60000u32;
-        y ^= y >> 18;
-
-        return y;
+        temper(y)
     }
 
     fn twist(&mut self) {
@@ -76,6 +70,15 @@ impl MT19937 {
         self.mti = 0;
     }
 
+}
+
+fn temper(i: u32) -> u32 {
+    let mut y = i;
+    y ^= y >> 11;
+    y ^= (y << 7) & 0x9d2c5680u32;
+    y ^= (y << 15) & 0xefc60000u32;
+    y ^= y >> 18;
+    y
 }
 
 #[cfg(test)]
@@ -132,6 +135,162 @@ pub mod tests {
             s -= 1;
         }
         assert_eq!(s, seed);
+    }
+
+    fn get_bits(u: u32, beg: usize, len: usize) -> u32 {
+        if len == 0 {panic!("len == 0")}
+        if beg > 32 {panic!("beg > 32")}
+        if beg + len > 32 {panic!("beg + len > 32")}
+
+        let mask: u32 = 0xffffffff;
+        (((mask << beg) >> (32 - len)) &
+         (u >> (32 - beg - len)))
+    }
+
+    #[test]
+    fn test_get_bits() {
+        assert_eq!(get_bits(3009615726, 10, 10), 0x231);
+        assert_eq!(get_bits(3009615726, 0, 10), 0x2cd);
+        assert_eq!(get_bits(3009615726, 10, 20), 0x8c5db);
+        assert_eq!(get_bits(3009615726, 0, 1), 1);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_get_bits_len_zero() {
+        get_bits(3009615726, 0, 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_get_bits_too_much() {
+        get_bits(3009615726, 32, 10);
+    }
+
+    /**
+    http://krypt05.blogspot.com/2015/10/reversing-shift-xor-operation.html
+    Note >> on unsigned ints fills with 0.
+
+    (1) y        a(10)  | b(10)   | c(10)   | d(2)
+    (2) y>>10    0(10)  | a(10)   | b(10)   | c(2)
+    (3) y^y>>10  a(10)  | a^b(10) | b^c(10) | c^d(2)
+
+    So to find (1) from (3), we need to xor a(10) with a^b(10) to get b(10),
+    then b(10) with b^c(10) to get c(10), and finally c(2) with c^d(2) to get
+    d(2).
+
+    Note we can apply the same reasoning for a left shift:
+    (1) y        a(2)   | b(10)   | c(10)   | d(10)
+    (2) y<<10    b(2)   | c(10)   | d(10)   | 0(10)
+    (3) y^y<<10  a^b(2) | b^c(10) | c^d(10) | d(10)
+     */
+    fn reverse_rshift_xor(u: u32, shift: usize) -> u32 {
+        let mut a = get_bits(u, 0, shift);
+        let mut res: u32 = a << (32-shift);
+        for i in 1..=32/shift {
+            let beg = i*shift;
+            let len = if beg + shift <= 32 {
+                shift
+            } else {
+                a >>= shift - 32%shift;
+                32-beg
+            };
+            let b = get_bits(u, beg, len);
+            a ^= b;
+            res |= a << (32 - beg - len);
+        }
+
+        res
+    }
+
+    #[test]
+    fn test_reverse_rshift_xor() {
+        assert_eq!(reverse_rshift_xor(3009615726, 10), 3008349343);
+        assert_eq!(reverse_rshift_xor(3076412325, 18), 3076423282);
+        assert_eq!(reverse_rshift_xor(3008339256, 17), 3008349343);
+        assert_eq!(reverse_rshift_xor(3598349337, 29), 3598349343);
+    }
+
+    /**
+    For y ^= (y << 7) & 0x9d2c5680u32;
+
+    (1) y        a(2)       | b(10)       | c(10)       | d(10)
+    (2) y<<10    b(2)       | c(10)       | d(10)       | 0(10)
+    (3) y^&      a^(b&m)(2) | b^(c&n)(10) | c^(d&o)(10) | d^(0&p)(10)
+
+    So d(10) is given. Knowing d and o, we can get c. With c and knowing d and
+    o, we can get b. And so on to the left.
+
+    TODO test boundaries (shift>16 for ex.)
+     */
+    fn reverse_lshift_and_xor(u: u32, shift: usize, m: u32) -> u32 {
+        let mut d = get_bits(u, 32-shift, shift);
+        let mut res: u32 = d;
+        for i in 1..=32/shift {
+            let end = 32-i*shift;
+            let len = if end > shift {
+                shift
+            } else {
+                d = get_bits(d , 32 - end, end);
+                end
+            };
+            let k = get_bits(u, end-len, len);
+            let o = get_bits(m, end-len, len);
+            d = k ^ (d & o);
+            res |= d << 32 - end;
+        }
+        res
+    }
+
+    #[test]
+    fn test_reverse_lshift_and_xor() {
+        assert_eq!(reverse_lshift_and_xor(577438062, 7, 0x9d2c5680), 3009615726);
+        assert_eq!(reverse_lshift_and_xor(1064241006, 10, 0x9d2c5680), 3009615726);
+        assert_eq!(reverse_lshift_and_xor(954537838, 15, 0xefc60000), 3009615726);
+    }
+
+    fn untemper(u: u32) -> u32 {
+        let y1 = reverse_rshift_xor(u, 18);
+        let y2 = reverse_lshift_and_xor(y1, 15, 0xefc60000);
+        let y3 = reverse_lshift_and_xor(y2, 7, 0x9d2c5680);
+        reverse_rshift_xor(y3, 11)
+    }
+
+    #[test]
+    fn test_untemper() {
+        assert_eq!(untemper(457947961), 3499211612);
+        assert_eq!(::rng::temper(untemper(3499211612)), 3499211612);
+        assert_eq!(untemper(4117162263), 457947961);
+    }
+
+    #[test]
+    fn test_clone_mt19937() {
+        let mut rng1 = MT19937::new(5489u32);
+        let mut samples1: Vec<u32> = Vec::new();
+        let mut state1: Vec<u32> = Vec::new();
+        for _ in 0..624 {
+            let r = rng1.rand_u32();
+            samples1.push(r);
+            state1.push(untemper(r));
+        }
+
+        let mut mt2 = [0u32; ::rng::N];
+        mt2.copy_from_slice(&state1[..624]);
+        let mut rng2 = MT19937 { mt: mt2, mti: 0 };
+        for i in 0..624 {
+            let rand2 = rng2.rand_u32();
+            assert_eq!(samples1[i], rand2);
+        }
+
+        /* « Stop and think for a second.  How would you modify MT19937 to make
+        this attack hard? What would happen if you subjected each tempered
+        output to a cryptographic hash? » Well, we would make the untemper
+        function irreversible. By using destructive operations like & or |. Not
+        sure how returning a hash of the tempered value as the result would
+        help: we could use a rainbow table for all u32 values, which would just
+        add one operation to the untempering. I wonder if the twist function
+        can be reversed. */
+
     }
 
 }
