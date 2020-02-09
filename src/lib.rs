056dc99fc4a82258e6c54128b8f4fc56843959dc
foudil <username@users.noreply.github.com>
2020-02-09 23:02:13 +0100

    Add ThreadPool lifted from rust-lang book

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub mod aes;
 pub mod b64;
 pub mod hmac;
 pub mod md4;
+pub mod par;
 pub mod pkcs;
 pub mod rng;
 mod set2;
---
+++ src/par.rs
@@ -0,0 +1,136 @@
+/// Lifted from https://doc.rust-lang.org/book/ch20-03-graceful-shutdown-and-cleanup.html
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>,
+}
+
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
+
+type Job = Box<dyn FnBox + Send + 'static>;
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &mut self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv().unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+
+                    job.call_box();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use par::*;
+
+    #[test]
+    fn test_thread_pool() {
+        let n_jobs = 8;
+        let n_workers = 4;
+
+        let pool = ThreadPool::new(n_workers);
+
+        let (tx, rx) = mpsc::channel();
+        for _ in 0..n_jobs {
+            let tx = tx.clone();
+            pool.execute(move || {
+                tx.send(1)
+                    .expect("channel will be there waiting for the pool");
+            });
+        }
+
+        assert_eq!(rx.iter().take(n_jobs).fold(0, |a, b| a + b), n_jobs);
+    }
+}
